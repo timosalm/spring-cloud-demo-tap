5e95c7dd020b5357f3df6de6ff6fdf7e933a82d7
Ryan Baxter <524254+ryanjbaxter@users.noreply.github.com>
2022-10-31 20:43:55 -0400

    pushing environment.prod.ts

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  baseHref: '/frontend/',
+  authConfig: {
+    issuer: 'http://authserver-1-dev-namespace.tap.ryanjbaxter.com',
+    clientId: 'dev-space_client-registration'
+  },
+  endpoints: {
+    orders: window.location.origin + '/services/order-service/api/v1/orders',
+    products: window.location.origin +  '/services/product-service/api/v1/products'
+  }
+};
