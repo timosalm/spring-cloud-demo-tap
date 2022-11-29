5aa6b5d5ce741ef55db58c16c12f30f8f82faf2f
Timo Salm <tsalm@vmware.com>
2022-11-29 10:16:04 +0100

    Disable CORS

--- gateway/src/main/java/com/example/gateway/WebSecurityConfiguration.java
+++ gateway/src/main/java/com/example/gateway/WebSecurityConfiguration.java
@@ -1,13 +1,14 @@
 package com.example.gateway;
 
+import org.springframework.cloud.gateway.config.GlobalCorsProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.Collections;
@@ -28,8 +29,14 @@ class WebSecurityConfiguration {
     }
 
     @Bean
+    CorsWebFilter corsFilter(CorsConfigurationSource corsConfigurationSource) {
+        return new CorsWebFilter(corsConfigurationSource);
+    }
+
+    @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         var configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
         configuration.setAllowCredentials(true);
         configuration.setAllowedOrigins(Collections.singletonList("*"));
         configuration.setAllowedMethods(Collections.singletonList("*"));
