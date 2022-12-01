062e2e3d6214efe92d6086bd6c75906d7b552235
Timo Salm <tsalm@vmware.com>
2022-12-01 11:16:08 +0100

    Fix CORS

--- gateway/src/main/java/com/example/gateway/WebSecurityConfiguration.java
+++ gateway/src/main/java/com/example/gateway/WebSecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.example.gateway;
 
+import org.springframework.cloud.gateway.config.GlobalCorsProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @EnableWebFluxSecurity
 @Configuration
@@ -13,12 +16,20 @@ class WebSecurityConfiguration {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
-                .csrf().disable()
                 .cors().and()
+                .csrf().disable()
                 .authorizeExchange(authorize -> authorize
                         .pathMatchers("/services/**").authenticated()
                         .pathMatchers("/**").permitAll()
                 )
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt).build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(
+            GlobalCorsProperties globalCorsProperties) {
+        var source = new UrlBasedCorsConfigurationSource();
+        globalCorsProperties.getCorsConfigurations().forEach(source::registerCorsConfiguration);
+        return source;
     }
 }
