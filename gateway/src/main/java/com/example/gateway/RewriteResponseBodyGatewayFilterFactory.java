3ea6368ec1b034923fa7073372d8544af3f82962
Timo Salm <52704922+timosalm@users.noreply.github.com>
2023-10-25 20:55:36 +0200

    Improvements

---
+++ gateway/src/main/java/com/example/gateway/RewriteResponseBodyGatewayFilterFactory.java
@@ -0,0 +1,42 @@
+package com.example.gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.rewrite.ModifyResponseBodyGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class RewriteResponseBodyGatewayFilterFactory extends AbstractGatewayFilterFactory<RewriteResponseBodyGatewayFilterFactory.Config> {
+
+    private final ModifyResponseBodyGatewayFilterFactory modifyResponseBodyFilterFactory;
+
+    public RewriteResponseBodyGatewayFilterFactory(ModifyResponseBodyGatewayFilterFactory modifyResponseBodyFilterFactory) {
+        super(Config.class);
+        this.modifyResponseBodyFilterFactory = modifyResponseBodyFilterFactory;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return modifyResponseBodyFilterFactory.apply(c -> c.setRewriteFunction(String.class, String.class, (serverWebExchange, body) -> {
+            config.getRewritesMap().forEach(body::replaceAll);
+            return Mono.just(body);
+        }));
+    }
+
+    public static class Config {
+        private String[] rewrites;
+
+        public Map<String, String> getRewritesMap() {
+            var rewritesMap = new HashMap<String,String>();
+            for (String rewrite: rewrites) {
+                rewritesMap.put(rewrite.split(":")[0], rewrite.split(":")[1]);
+            }
+            return rewritesMap;
+        }
+
+    }
+}
